e5f850f4bd7bd1d243fe49389199a8052de5eed4
glenluersman <glen.luersman@gmail.com>
2021-11-13 15:00:25 -0500

    added password loop

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
 // Assignment code here
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var lowerCases = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCases = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var specialChars = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
-
-var length = "";
-var number;
-var lower;
-var upper;
-var special;
+var number = "0123456789";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var characters = "";
 
 function generatePassword() {
-  var length = window.prompt("What is the length of your password? Length must be between 8 and 128 characters.");
+  var length = Number(prompt("What is the length of your password? Length must be between 8 and 128 characters."));
     while (length <= 7  || length >= 129) {
       window.alert("Password length must be between 8 and 128 characters try again.");
-      window.alert("Your password will contain " + length + " characters.");
       return length;
     };
     console.log("Pasword length is " + length);
     
-    var number = window.confirm("Would you like numbers in your password?");
-    var lower = window.confirm("Would you like lower case letters in your password?");
-    var upper = window.confirm("Would you like upper case letters in you password?");
-    var special = window.confirm("Would you like special characters in your password?");
-      while (number === false && lower === false && upper === false && special === false) {
+    var numbers = window.confirm("Would you like numbers in your password?");
+    var lowerCase = window.confirm("Would you like lower case letters in your password?");
+    var upperCase = window.confirm("Would you like upper case letters in you password?");
+    var specialChars = window.confirm("Would you like special characters in your password?");
+      while (numbers === false && lowerCase === false && upperCase === false && specialChars === false) {
         window.alert("You must choose at least one perameter!");
-        var number = window.confirm("Would you like numbers in your password?");
-        var lower = window.confirm("Would you like lower case letters in your password?");
-        var upper = window.confirm("Would you like upper case letters in you password?");
-        var special = window.confirm("Would you like special characters in your password?");
+        var numbers = window.confirm("Would you like numbers in your password?");
+        var lowerCase = window.confirm("Would you like lower case letters in your password?");
+        var upperCase = window.confirm("Would you like upper case letters in you password?");
+        var specialChars = window.confirm("Would you like special characters in your password?");
       };
+
+      if (numbers) {
+        characters += number;
+      };
+      if (lowerCase) {
+        characters += lower;
+      };
+      if (upperCase) {
+        characters += upper;
+      };
+      if (specialChars) {
+        characters += special;
+      };
+
+  var newPassword = "";
+    for (var i = 0; i < length; i++) {
+      newPassword = newPassword + characters[Math.floor(Math.random() * characters.length)];
+      console.log(newPassword);
+    };
+    return newPassword;
 };
   
   // Write password to the #password input
